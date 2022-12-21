e7a3f7bbdb6b593d2156ab5c48e8cd53d4819732
Miguel101177 <mangelsany6@gmail.com>
2022-12-21 09:28:50 -0300

    Se agregan páginas de login y register, Claso 21

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.listen(3000,()=>{
     console.log("Servidor iniciado");
 })
 app.use(express.static(path.join(__dirname,"public")));
+
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"))
 })
