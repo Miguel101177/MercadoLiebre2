a1d33ce1a2e7271be9bf083ad7cd7a156fb78ff8
Miguel101177 <mangelsany6@gmail.com>
2023-01-02 20:55:28 -0300

    Se actualiza app.js y package.json para Render

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const app=express();
 const path=require("path");
-
-app.listen(3000,()=>{
-    console.log("Servidor iniciado");
+const PORT=process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log(`Se prendió en el puerto ${PORT}`);
 })
 app.use(express.static(path.join(__dirname,"public")));
 
